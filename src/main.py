eb243de0fa0e78704bc3279da3044db32f2d9858
Jose Luis <jocefuo@gmail.com>
2025-07-13 15:57:27 +0200

    validation function

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+def validacionMensaje(mensaje, cofre):
+    mensaje = mensaje.replace(" ", "").lower()
+    cofre = cofre.lower()
+    
+    letras_mensaje = Counter(mensaje)
+    letras_cofre = Counter(cofre)
+    
+    for letra, cantidad in letras_mensaje.items():
+        if letras_cofre[letra] < cantidad:
+            return False
+    return True
+
+
+print(validacionMensaje("hola mundo", "holamundo"))
+print(validacionMensaje("test", "dasiknujfewrnoiuj"))
